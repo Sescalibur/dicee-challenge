401434f15744484af5c3b870d1035113c03dfbaf
Samet Emin Sevil <89660530+Sescalibur@users.noreply.github.com>
2023-10-26 14:16:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function randomNumber(){
-    var random1 = Math.floor(Math.random()*6)+1;
-    document.querySelector(".img1").setAttribute("src","./images/dice"+random1+".png");
-    var random2 = Math.floor(Math.random()*6)+1;
-    document.querySelector(".img2").setAttribute("src","./images/dice"+random2+".png");
-    if(random1>random2){
-        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! ";
-    }
-    else if(random2>random1){
-        document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-    }
-    else{
-        document.querySelector("h1").innerHTML = "🚩Draws! 🚩";
-    }
+var random1 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","./images/dice"+random1+".png");
+var random2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","./images/dice"+random2+".png");
+if(random1>random2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! ";
 }
+else if(random2>random1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "🚩Draws! 🚩";
+}
+
+    
